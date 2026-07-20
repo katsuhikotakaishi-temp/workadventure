f1a484a61f5860baf71ad184f974ed3183ceefec
katsuhikotakaishi-temp <katsuhiko.takaishi@temperance-st.biz>
2026-07-21 02:52:20 +0900

    feat: export doorbell card icon

--- play/src/front/Components/Icons.ts
+++ play/src/front/Components/Icons.ts
@@ -48,6 +48,7 @@ export { default as IconFolder } from "~icons/tabler/folder";
 export { default as IconUnMute } from "~icons/tabler/volume";
 export { default as IconMute } from "~icons/tabler/volume3";
 export { default as IconRestore } from "~icons/tabler/restore";
+export { default as IconBell } from "~icons/tabler/bell";
 export { default as IconBellOff } from "~icons/tabler/bell-off";
 export { default as IconSearch } from "~icons/tabler/search";
 export { default as IconMailBox } from "~icons/tabler/mailbox";
